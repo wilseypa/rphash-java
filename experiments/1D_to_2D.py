e8568234976844110362116610ae242d971855a8
Anindya-Moitra <id.anindya@gmail.com>
2015-07-29 19:30:56 -0400

    Update 1D_to_2D.py

--- experiments/1D_to_2D.py
+++ experiments/1D_to_2D.py
@@ -10,7 +10,7 @@ fc.write('\n')
 
 for i in range(1, int(arr[0])+1):
     for j in range(1, int(arr[1])+1):
-        fc.write(str(int(arr[index])) + ',')
+        fc.write(str(arr[index]) + ',')
         index += 1
     fc.write('\n')
 
