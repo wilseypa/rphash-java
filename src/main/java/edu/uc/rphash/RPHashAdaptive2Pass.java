8c2340e1b1771a3b9be08eee969f8f40e8329241
lee@dennis <leecarraher@gmail.com>
2017-01-24 23:23:43 -0500

    adding integer based golay decode, incorporated with adaptive lsh. seems to work well.

--- src/main/java/edu/uc/rphash/RPHashAdaptive2Pass.java
+++ src/main/java/edu/uc/rphash/RPHashAdaptive2Pass.java
@@ -1,9 +1,6 @@
 package edu.uc.rphash;
 
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,16 +8,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
 
-import org.apache.commons.lang3.ArrayUtils;
-
 import edu.uc.rphash.Readers.RPHashObject;
 import edu.uc.rphash.Readers.SimpleArrayReader;
+import edu.uc.rphash.decoders.Golay2;
 import edu.uc.rphash.projections.DBFriendlyProjection;
 import edu.uc.rphash.projections.Projector;
 import edu.uc.rphash.tests.StatTests;
 import edu.uc.rphash.tests.clusterers.Agglomerative3;
 import edu.uc.rphash.tests.generators.GenerateData;
-import edu.uc.rphash.util.VectorUtil;
 
 public class RPHashAdaptive2Pass implements Clusterer, Runnable {
 
@@ -54,11 +49,12 @@ public class RPHashAdaptive2Pass implements Clusterer, Runnable {
 			}
 		}
 		for (int j = 0; j < ret.length; j++) {
-			ret[j] = ret[j] / ((float)X.size());
+			ret[j] = ret[j] / ((float) X.size());
 		}
 		return ret;
 	}
 
+	Golay2 gol = new Golay2();
 	/*
 	 * super simple hash algorithm, reminiscient of pstable lsh
 	 */
@@ -69,7 +65,8 @@ public class RPHashAdaptive2Pass implements Clusterer, Runnable {
 			if (x[i] > 0)
 				s += 1;
 		}
-		return s;
+		
+		return (long)gol.golay_decode((int)s);
 	}
 
 	/*
@@ -95,34 +92,34 @@ public class RPHashAdaptive2Pass implements Clusterer, Runnable {
 			}
 		}
 	}
-	
 
 	/*
 	 * X - data set k - canonical k in k-means l - clustering sub-space Compute
 	 * density mode via iterative deepening hash counting
 	 */
 	public ArrayList<float[]> findDensityModes() {
-		//HashMap<Long, Long> IDAndCount = new HashMap<>();
+		// HashMap<Long, Long> IDAndCount = new HashMap<>();
 		HashMap<Long, List<float[]>> IDAndCent = new HashMap<>();
 
 		// #create projector matrixs
 		DBFriendlyProjection projector = new DBFriendlyProjection(so.getdim(),
 				so.getDimparameter());
-		
-		//VectorUtil.simpleSave(projector.M,"/home/lee/Desktop/reclsh/M");
-		//VectorUtil.simpleSave(projector.P,"/home/lee/Desktop/reclsh/P");
+
+		// VectorUtil.simpleSave(projector.M,"/home/lee/Desktop/reclsh/M");
+		// VectorUtil.simpleSave(projector.P,"/home/lee/Desktop/reclsh/P");
 
 		// #process data by adding to the counter
 		for (float[] x : so.getRawData()) {
-			addtocounter(x, projector, IDAndCent,
-					so.getDimparameter());
+			addtocounter(x, projector, IDAndCent, so.getDimparameter());
 		}
 
 		// next we want to prune the tree by parent count comparison
 		// follows breadthfirst search
 		HashMap<Long, Long> denseSetOfIDandCount = new HashMap<Long, Long>();
 		for (Long h : new TreeSet<Long>(IDAndCent.keySet())) {
-			if (h > 1 && 2 * IDAndCent.get(h).size() > IDAndCent.get(h >>> 1).size()) {
+			if (h > 1
+					&& 2 * IDAndCent.get(h).size() > IDAndCent.get(h >>> 1)
+							.size()) {
 				denseSetOfIDandCount.put(h, new Long(IDAndCent.get(h).size()));
 				// remove parent of denser child
 				denseSetOfIDandCount.remove(h >>> 1);
@@ -134,10 +131,9 @@ public class RPHashAdaptive2Pass implements Clusterer, Runnable {
 				.entrySet()
 				.stream()
 				.sorted(Map.Entry.<Long, Long> comparingByValue().reversed())
-				.limit(so.getk())
+				.limit(so.getk()*so.getk())
 				.forEachOrdered(
 						x -> estcents.add(medoid(IDAndCent.get(x.getKey()))));
-		//estcents.stream().forEach(x->VectorUtil.prettyPrint(x));
 		return estcents;
 	}
 
@@ -146,21 +142,23 @@ public class RPHashAdaptive2Pass implements Clusterer, Runnable {
 		centroids = new ArrayList<>();
 		for (float[] f : rawcent)
 			centroids.add(new Centroid(f, 1));
-		 //centroids = new Agglomerative3(rawcent, so.getk()).getCentroids();
+		centroids = new Agglomerative3(rawcent, so.getk()).getCentroids();
 	}
 
-	public static void main(String[] args) throws FileNotFoundException, IOException {
+	public static void main(String[] args) throws FileNotFoundException,
+			IOException {
 
 		int k = 10;
 		int d = 1000;
 		int n = 10000;
-		float var = 1.f;
-		for (float f = var; f < 1.01; f += .1f) {
-			for (int i = 0; i < 1; i++) {
+		float var = .1f;
+		for (float f = var; f < 2.01; f += .1f) {
+			for (int i = 0; i < 5; i++) {
 				GenerateData gen = new GenerateData(k, n / k, d, f, true, 1f);
-				//gen.writeCSVToFile(new File("/home/lee/Desktop/reclsh/in.csv"));
+				// gen.writeCSVToFile(new
+				// File("/home/lee/Desktop/reclsh/in.csv"));
 				RPHashObject o = new SimpleArrayReader(gen.data, k);
-				o.setDimparameter(32);
+				o.setDimparameter(24);
 				RPHashAdaptive2Pass rphit = new RPHashAdaptive2Pass(o);
 				long startTime = System.nanoTime();
 				List<Centroid> centsr = rphit.getCentroids();
